ef2e7c3b197383a8fa0b8a175636a844d1159e50
AAA530 <dhairyapatelad@gmail.com>
2020-05-11 16:16:15 +0530

    added passport configuration

--- app.js
+++ app.js
@@ -2,12 +2,36 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const socketio = require('socket.io')
 const mongoose = require('mongoose')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const passportLocalMongoose = require('passport-local-mongoose')
 const http = require('http')
+
+const User = require('./models/user')
 
 
 const app = express()
 const server = http.createServer(app)        //server for app
 const io = socketio(server)                 // server for socket.io
+
+app.use(require('express-session')({
+    secret : "Zilch is new chatting app",
+    resave : false,
+    saveUninitialized : false
+}))
+
+//passport configuration
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
 
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
@@ -18,9 +42,7 @@ mongoose.connect("mongodb://localhost/Private_chats",()=>{
     console.log("connected")                                    //connecting mongodb database
 });
 
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended:true}));
+
 
 //Routes
 app.get('/',(req,res)=>{
---
+++ models/user.js
@@ -0,0 +1,15 @@
+var mongoose               = require('mongoose'),
+    passportLocalMongoose  = require('passport-local-mongoose');
+
+// User Schema
+
+var UserSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+UserSchema.plugin(passportLocalMongoose);
+
+
+var User = mongoose.model("User", UserSchema);
+
+module.exports = User;
